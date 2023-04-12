b618f887222b0cc2e7e62ed6665ca4b78ab86075
YoussefHlem <80886022+HlemoXGamer@users.noreply.github.com>
2023-04-12 08:55:07 +0200

    edit

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Bubbles</title>
-        <link rel="icon" href="../public/logo.png" />
       </Head>
       <body>
         <Main />
